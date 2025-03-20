014ab7901111f02e9e47f6366c113acbeebb8e4a
eckserah <eckserah@gmail.com>
2025-03-20 01:27:50 -0400

    Update CodeEditor to 1.31

---
+++ wiki/extensions/CodeEditor/Gruntfile.js
@@ -0,0 +1,49 @@
+/*!
+ * Grunt file
+ *
+ * @package CodeEditor
+ */
+
+/* eslint-env node */
+
+module.exports = function ( grunt ) {
+	var conf = grunt.file.readJSON( 'extension.json' );
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+	grunt.loadNpmTasks( 'grunt-jsonlint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				cache: true
+			},
+			all: [
+				'**/*.js',
+				'!node_modules/**',
+				'!modules/ace/**',
+				'!vendor/**'
+			]
+		},
+		stylelint: {
+			all: [
+				'**/*.css',
+				'**/*.less',
+				'!node_modules/**',
+				'!modules/ace/**',
+				'!vendor/**'
+			]
+		},
+		banana: conf.MessagesDirs,
+		jsonlint: {
+			all: [
+				'**/*.json',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		}
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
