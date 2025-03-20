cb03f1786e517bbf11ffc0e7942e7618bb8845d1
eckserah <eckserah@gmail.com>
2025-03-20 01:25:00 -0400

    Update Cite to 1.31

--- wiki/extensions/Cite/Gruntfile.js
+++ wiki/extensions/Cite/Gruntfile.js
@@ -13,8 +13,13 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-eslint' );
 	grunt.loadNpmTasks( 'grunt-jsonlint' );
 	grunt.loadNpmTasks( 'grunt-stylelint' );
+	grunt.loadNpmTasks( 'grunt-svgmin' );
+
 	grunt.initConfig( {
 		eslint: {
+			options: {
+				cache: true
+			},
 			all: [
 				'**/*.js',
 				'{.jsduck,build}/**/*.js',
@@ -38,9 +43,44 @@ module.exports = function ( grunt ) {
 				'!node_modules/**',
 				'!vendor/**'
 			]
+		},
+		// SVG Optimization
+		svgmin: {
+			options: {
+				js2svg: {
+					pretty: true,
+					multipass: true
+				},
+				plugins: [ {
+					cleanupIDs: false
+				}, {
+					removeDesc: false
+				}, {
+					removeRasterImages: true
+				}, {
+					removeTitle: false
+				}, {
+					removeViewBox: false
+				}, {
+					removeXMLProcInst: false
+				}, {
+					sortAttrs: true
+				} ]
+			},
+			all: {
+				files: [ {
+					expand: true,
+					cwd: 'modules/ve-cite/icons',
+					src: [
+						'**/*.svg'
+					],
+					dest: 'modules/ve-cite/icons/',
+					ext: '.svg'
+				} ]
+			}
 		}
 	} );
 
-	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'jsonlint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'jsonlint', 'banana', 'svgmin' ] );
 	grunt.registerTask( 'default', 'test' );
 };
